ad9eefd046effc7cb0967d8138247f5275e569d0
Phil Spindler <phil.spindler+equus@equuscs.com>
2026-07-11 05:28:08 -0500

    transition to composite usb device

--- nucleo/src/usb_io.rs
+++
@@ -1,9 +0,0 @@
-//! USB <-> Serial / CDC Interface
-
-use embassy_stm32::peripherals;
-use embassy_stm32::usb::Driver;
-
-#[embassy_executor::task(pool_size = 1)]
-pub async fn usb_task(driver: Driver<'static, peripherals::USB>) {
-    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
-}
